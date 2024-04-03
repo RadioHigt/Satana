734216066c04c8534c65027bdb51eec16d55eff8
Егор Чернов <gr612_cheepa@lab-207-4.tomtit-tomsk.ru>
2024-04-03 16:08:55 +0700

    Merge new title from teammate

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Привет, я Клоун, веселая программа");
+    Console.WriteLine("Ха-ха-ха");
+    Console.WriteLine("Ваш калькулятор");
+    Console.WriteLine("Введите первое число");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine("Укажите функцию");
+    string c = Console.ReadLine();
+    if (c == "+")
+    {
+        plus(a,b);
+    }
+    else if(c == "-")
+    {
+        minus(a,b);
+    }
+    else if(c == "/")
+    {
+        division(a,b);
+    }
+    else if(c == "*")
+    {
+        multiplication(a,b);
+    }
+    else
+    {
+      Console.WriteLine("Нет такой функции");  
+    }
+  }
+  public static void plus(int a, int b)
+  {
+      Console.WriteLine( a+b);
+  }
+  public static void minus(int a,int b)
+  {
+      Console.WriteLine(a+b);
+  }
+}
